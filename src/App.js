fe0c833f812e80750079013ef75aa8065ec6e895
model-vue-controller <prewalking@gmail.com>
2020-09-23 22:29:43 -0600

    adding class objects to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clarkson',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to your new react app';
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+
+        {/* { list.map(function(item) { */}
+        { list.map(item =>
+          <div key={item.objectID}>
+            <span> <a href={item.url}>{item.title}</a> </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+          </div>
+        )}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
